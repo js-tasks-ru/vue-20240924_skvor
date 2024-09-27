1892b8d5270ed7ceabf76d0961a10c974215d498
Skvorcov <gskvorcov@mmtr.ru>
2024-09-27 22:32:10 +0300

    Solve 01-basics/10-create-app Solve 01-basics/20-weather

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,9 @@
 import { defineComponent, createApp } from 'vue'
+
+const options = {
+  dateStyle:'long'
+};
+createApp({
+  name: 'todayDate',
+  template: 'Сегодня ' + new Date().toLocaleDateString(navigator.language, options),
+}).mount('#app')
